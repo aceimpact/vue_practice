8e6690c2d41a25547345e2b0326b0638fbe336c4
aceimpact <ace4869kei@gmail.com>
2022-04-27 03:42:07 +0900

    追加

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+// Vue.component("comp-board", {
+// 	template: "<div>Message Board</div>",
+// });
+
+// Vue.component("comp-form", {
+// 	template: '<div>Form<textarea v-model="message"></textarea></div>',
+// 	data: function () {
+// 		return {
+// 			message: "",
+// 		};
+// 	},
+// });
+
 new Vue({
 	el: "#app",
 	data: {
-		list: [],
+		order: false,
+		list: [
+			{ id: 1, name: "りんご", price: 100 },
+			{ id: 2, name: "ばなな", price: 200 },
+			{ id: 3, name: "いちご", price: 300 },
+		],
 	},
-	watch: {
-		list: function () {
-			console.log("通常:", this.$refs.list.offsetHeight);
-
-			this.$nextTick(function () {
-				console.log("nextTick:", this.$refs.list.offsetHeight);
-			});
+	computed: {
+		sortedList: function () {
+			return _.orderBy(this.list, "price", this.order ? "desc" : "asc");
 		},
 	},
 });
